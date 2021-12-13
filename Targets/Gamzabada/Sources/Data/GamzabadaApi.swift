78e61cf57878ab3b299b350e21990cea92177b3e
kiryun <rlgus1205@naver.com>
2021-12-13 21:39:30 +0900

    sns login에 관련된 기능을 러프하게 구현

---
+++ Targets/Gamzabada/Sources/Data/GamzabadaApi.swift
@@ -0,0 +1,9 @@
+//
+//  GamzabadaApi.swift
+//  Gamzabada
+//
+//  Created by Wimes on 2021/12/13.
+//  Copyright © 2021 takeapotato.com. All rights reserved.
+//
+
+import Foundation
---
+++ Targets/Gamzabada/Sources/Scene/Login/LoginViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewModel.swift
+//  Gamzabada
+//
+//  Created by Wimes on 2021/12/13.
+//  Copyright © 2021 takeapotato.com. All rights reserved.
+//
+
+import Foundation
