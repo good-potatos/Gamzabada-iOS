ec0f0c6f1b57549a4e2c5f8ef112cd417e10f929
kiryun <rlgus1205@naver.com>
2022-02-06 21:14:25 +0900

    apply SwiftFormatter

--- Effects/Sources/Effects/API/Builder/UrlBuilder.swift
+++ Effects/Sources/Effects/API/Builder/UrlBuilder.swift
@@ -1,27 +1,27 @@
 //
 //  UrlBuilder.swift
-//  
+//
 //
 //  Created by Wimes on 2022/02/05.
 //
 
 import Foundation
 
-struct UrlBuilder{
+struct UrlBuilder {
     var object: GamzabadaApi
-    
-    var url: URL{
+
+    var url: URL {
         var components = URLComponents()
-        
+
         components.scheme = "https"
         components.host = self.object.baseUrl
         components.path = self.object.path
         components.queryItems = []
-        
-        self.object.queryParameter?.forEach{
+
+        self.object.queryParameter?.forEach {
             components.queryItems?.append(URLQueryItem(name: $0.key, value: $0.value))
         }
-        
+
         return components.url!
     }
 }
