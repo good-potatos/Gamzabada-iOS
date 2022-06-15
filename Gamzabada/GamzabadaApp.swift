fd2f4513abd5504fb8b080593255cd500c87736e
dp221125 <dp221125@naver.com>
2022-06-15 23:15:49 +0900

    First Commit

---
+++ Gamzabada/GamzabadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamzabadaApp.swift
+//  Gamzabada
+//
+//  Created by Milkyo on 2022/06/15.
+//
+
+import SwiftUI
+
+@main
+struct GamzabadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
