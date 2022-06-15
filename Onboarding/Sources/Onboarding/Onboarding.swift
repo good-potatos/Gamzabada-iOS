575cad574d7f82777717141a0b06e66938735c00
dp221125 <dp221125@naver.com>
2022-06-16 00:10:50 +0900

    Onboarding을 추가합니다.

---
+++ Onboarding/Sources/Onboarding/Onboarding.swift
@@ -0,0 +1,5 @@
+public struct Onboarding {
+    public private(set) var text = "Hello, World!"
+
+    public init() {}
+}
---
+++ Onboarding/Tests/OnboardingTests/OnboardingTests.swift
@@ -0,0 +1,11 @@
+@testable import Onboarding
+import XCTest
+
+final class OnboardingTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Onboarding().text, "Hello, World!")
+    }
+}
