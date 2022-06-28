547a27e2b0f2cea19100d1cc00001f406ae8eff3
dp221125 <dp221125@naver.com>
2022-06-28 21:03:20 +0900

    [resolved#31] DEBUG용 디펜더시를 가져오는 모듈을 생성합니다

--- ProxyModule/Package.swift
+++ ProxyModule/Package.swift
@@ -17,20 +17,14 @@ let package = Package(
             url: "https://github.com/pointfreeco/swift-composable-architecture",
             .upToNextMajor(from: "0.36.0")
         ),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.49.7"),
         .package(url: "https://github.com/CombineCommunity/CombineExt.git", from: "1.0.0"),
-        .package(url: "https://github.com/FLEXTool/FLEX.git", from: "4.3.0"),
-        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "1.9.0"),
-
     ],
     targets: [
         .target(
             name: "ProxyModule",
             dependencies: [
                 .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-                .product(name: "FLEX", package: "FLEX"),
                 .product(name: "CombineExt", package: "CombineExt"),
-                .product(name: "SwiftyBeaver", package: "SwiftyBeaver"),
             ]
         ),
         .testTarget(
