2a9f3d9ee7b0a1db9d048d3dfa1849d228c57f9c
dev-wimes <dev.wimes@gmail.com>
2022-06-28 22:01:37 +0900

    Feature Package 생성

---
+++ Feature/Sources/Feature/Feature.swift
@@ -0,0 +1,14 @@
+//
+//  Feature.swift
+//
+//
+//  Created by Wimes on 2022/06/28.
+//
+
+import Foundation
+
+public struct Feature {
+    public private(set) var text = "Hello, World!"
+
+    public init() {}
+}
